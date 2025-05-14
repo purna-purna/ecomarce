a46458430f42077693880cc422cf78aa85197a2e
purna-purna <purnap271@gmail.com>
2025-05-14 12:00:01 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ecommarce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ecommarce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/providers/products_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductsProvider with ChangeNotifier {
+  final List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Blue Jeans',
+      description: 'A nice pair of blue jeans.',
+      price: 59.99,
+      imageUrl:
+          'https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+    ),
+  ];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+}
